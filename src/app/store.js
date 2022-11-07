c4e2ebfffa3a6ca48023a25b1879acb1b13287f6
Harsh Jain <66999104+Harshjain007@users.noreply.github.com>
2022-11-07 12:29:39 +0530

    Cleanup

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import rootReducers from '../features/reducers';
 
 export const store = configureStore({
